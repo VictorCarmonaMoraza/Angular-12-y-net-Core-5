39427e3b385bff9c7e2edff4d0988a0ddfdb27bf
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-01-10 20:46:47 +0100

    26-Parametros - Haciendo Componentes Reutilizables

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -8,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
 
   title = 'front-end';
-
+  peliculasEnCines;
+  peliculasProximosEstrenos;
 
   ngOnInit(): void {
+    setTimeout(() => {
+      this.peliculasEnCines = [{
+        titulo: 'Spider-Man',
+        fechaLanzamiento: new Date(),
+        precio: 1400.99
+      },
+      {
+        titulo: 'Moana',
+        fechaLanzamiento: new Date('2016-11-14'),
+        precio: 300.99
+      }]
+    }, 1000);
+
+    setTimeout(() => {
+      this.peliculasProximosEstrenos = [{
+        titulo: 'Avenger',
+        fechaLanzamiento: new Date(),
+        precio: 1400.99
+      },
+      {
+        titulo: 'Inception',
+        fechaLanzamiento: new Date('2016-11-14'),
+        precio: 300.99
+      }, {
+        titulo: 'Robocop',
+        fechaLanzamiento: new Date('2016-11-14'),
+        precio: 300.99
+      }]
+    }, 3000)
   }
+
 
 }
--- front-end/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
+++ front-end/src/app/peliculas/listado-peliculas/listado-peliculas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listado-peliculas',
@@ -7,24 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadoPeliculasComponent implements OnInit {
 
-  peliculas;
+  //Decorador para recibir parametros
+  @Input() peliculas;
 
   constructor() { }
 
   ngOnInit(): void {
 
-    setTimeout(() => {
-      this.peliculas = [{
-        titulo: 'Spider-Man',
-        fechaLanzamiento: new Date(),
-        precio: 1400.99
-      },
-      {
-        titulo: 'Moana',
-        fechaLanzamiento: new Date('2016-11-14'),
-        precio: 300.99
-      }]
-    }, 1000)
   }
 
 }
