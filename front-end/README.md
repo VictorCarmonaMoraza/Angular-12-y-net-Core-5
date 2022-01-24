d2f07c46e484c18f00426518dd805aa93a9a5490
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-01-24 21:45:54 +0100

    53 - Filtro de peliculas-Lógica

--- front-end/README.md
+++ front-end/README.md
@@ -1,4 +1,4 @@
-## 52 - Filtro de Películas - Plantilla
+## 53 - Filtro de peliculas-Lógica
 
 Este es un select hardcodeado con datos
 
@@ -13,3 +13,33 @@ Este es un select hardcodeado con datos
 </form>
 
 Se han creado los campos de titulo, genero y dos checkbox para proximos estrenos y peliculas que estan en cines.
+
+El método indexOf()devuelve el índice, dentro del objeto String que realiza la llamada, de la primera ocurrencia del valor especificado, comenzando la búsqueda desde indiceDesde; o -1 si no se encuentra dicho valor.
+
+Para limpiar nuestro formulario hemos hecho
+
+  1-Creamos una variable para guardar nuestro formulario
+  formularioOriginal = {
+    titulo: '',
+    generoId: 0,
+    proximosEstrenos: false,
+    enCines: false
+  }
+
+  2-Inicializamos con el formulario original
+  ngOnInit(): void {
+    //Campos del formulario
+    this.formPelicula = this.formBuilder.group(this.formularioOriginal);
+
+  
+  3-Creamos el boton limpiar para que lance la funcion limpiar
+  <div class="contenedor-limpiar">
+    <button mat-flat-button (click)="limpiar()" color="warn">Limpiar</button>
+  </div>
+
+  4-Creamos el metodo de la funcion limpiar
+  //Metodo que se llama mediante el boton limpiar
+  limpiar() {
+    //Limpiaremos nuestro formulario.Le pasamos el formulario original a nuestro formulario
+    this.formPelicula.patchValue(this.formularioOriginal);
+  }
--- front-end/src/app/peliculas/filtro-peliculas/filtro-peliculas.component.ts
+++ front-end/src/app/peliculas/filtro-peliculas/filtro-peliculas.component.ts
@@ -15,22 +15,64 @@ export class FiltroPeliculasComponent implements OnInit {
     { id: 3, nombre: 'Comedia' }
   ];
 
+  peliculasAEnviar = [
+    { titulo: 'Spider-Man: Far From Home', enCines: false, proximosEstrenos: true, generos: [1, 2], poster: 'https://as01.epimg.net/us/imagenes/2019/06/20/tikitakas/1561057925_744437_1561060071_noticia_normal_recorte1.jpg' },
+    { titulo: 'Moana: Far From Home', enCines: true, proximosEstrenos: false, generos: [3], poster: 'https://ep01.epimg.net/verne/imagenes/2015/10/08/articulo/1444317349_787548_1444414255_noticia_normal.jpg' },
+    { titulo: 'Inception', enCines: false, proximosEstrenos: false, generos: [1, 3], poster: 'https://2.bp.blogspot.com/_dAGq_7k2IB8/THlCDA3uAWI/AAAAAAAACLg/ShbduPuEw-s/s1600/inception_ver13_xlg.jpg' }
+  ];
+  //Para guardar una copia
+  peliculasOriginal = this.peliculasAEnviar;
+  //Nuestro formulario de inicio
+  formularioOriginal = {
+    titulo: '',
+    generoId: 0,
+    proximosEstrenos: false,
+    enCines: false
+  }
 
   constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     //Campos del formulario
-    this.formPelicula = this.formBuilder.group({
-      titulo: '',
-      generoId: 0,
-      proximosEstrenos: false,
-      enCines: false
-    });
+    this.formPelicula = this.formBuilder.group(this.formularioOriginal);
+    
+    //Esto devuelve un observable
+    //Cada vez que haya un cambio en el formulario saltara un evento
+    this.formPelicula.valueChanges
+      .subscribe(valores => {
+        console.log(valores);
+        //Hay que limpiar cada vez que se haga una busqueda
+        this.peliculasAEnviar = this.peliculasOriginal;
+        //Llamamos al metodo con los cambios que se vayan produciendo
+        this.buscarPeliculas(valores);
+    })
   }
 
-  //Metod que se llama mediante el boton limpiar
+  //Metodo que se llama mediante el boton limpiar
   limpiar() {
+    //Limpiaremos nuestro formulario.Le pasamos el formulario original a nuestro formulario
+    this.formPelicula.patchValue(this.formularioOriginal);
+  }
 
+  buscarPeliculas(valores: any) {
+    if (valores.titulo) {
+      //Si la pelicula es distinta de menos es que hemos encontrado la pelicula
+      //Si lo que el usuario ha escrito se encuentra dentro del titulo
+      this.peliculasAEnviar = this.peliculasAEnviar
+        .filter(pelicula=>pelicula.titulo.indexOf(valores.titulo)!==-1)
+    }
+    if (valores.generoId !==0) {
+      this.peliculasAEnviar = this.peliculasAEnviar
+        .filter(pelicula => pelicula.generos.indexOf(valores.generoId) !== -1)
+    }
+    if (valores.proximosEstrenos ) {
+      this.peliculasAEnviar = this.peliculasAEnviar
+        .filter(pelicula => pelicula.proximosEstrenos)
+    }
+    if (valores.enCines) {
+      this.peliculasAEnviar = this.peliculasAEnviar
+        .filter(pelicula => pelicula.enCines)
+    }
   }
 
 }
