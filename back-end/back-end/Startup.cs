b4b7297b66bf607a8ca35b28599622211495adb4
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-02-16 12:06:10 +0100

    82 - Middleware

--- back-end/back-end/Startup.cs
+++ back-end/back-end/Startup.cs
@@ -2,6 +2,7 @@ using back_end.Interfaces;
 using back_end.Repositorios;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -11,6 +12,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -37,8 +39,42 @@ namespace back_end
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger<Startup>logger)
         {
+            //Guardamos todas las repuesta de log de los clientes
+            app.Use(async (context, next) =>
+            {
+                using (var swapStream = new MemoryStream())
+                {
+                    var respuestaOriginal = context.Response.Body;
+                    context.Response.Body = swapStream;
+
+                    //Con esto decimos que queremos que continue la ejecucion del pipeline
+                    await next.Invoke();
+
+                    swapStream.Seek(0, SeekOrigin.Begin);
+                    string respuesta = new StreamReader(swapStream).ReadToEnd();
+                    swapStream.Seek(0, SeekOrigin.Begin);
+
+                    await swapStream.CopyToAsync(respuestaOriginal);
+                    context.Response.Body = respuestaOriginal;
+
+                    logger.LogInformation(respuesta);
+
+                }
+            });
+
+            //Esto solo se ejecuta si accedemos a una url especifica
+            app.Map("/mapa1", (app) =>
+            {
+                app.Run(async context =>
+                {
+                    await context.Response.WriteAsync("Estoy interceptando el pipeline");
+                });
+            });
+
+            
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
