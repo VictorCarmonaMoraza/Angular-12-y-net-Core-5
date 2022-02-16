e66b9016d5cbbc393c659de310ed1e0e2c29bd57
Victor Carmona <VictorCarmona9035@hotmail.com>
2022-02-16 07:50:04 +0100

    78 - Validaciones Personalizadas

--- back-end/back-end/Entidades/Genero.cs
+++ back-end/back-end/Entidades/Genero.cs
@@ -1,14 +1,31 @@
 ﻿//Una entidad representa una tabla en nuestra base de datos
+using back_end.Validaciones;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace back_end.Entidades
 {
-    public class Genero
+    public class Genero:IValidatableObject
     {
         public int Id { get; set; }
         [Required(ErrorMessage ="El campo {0} es requerido")]
         [StringLength(maximumLength:10,
         ErrorMessage = "El campo {0} no debe tener mas de 10 caracteres")]
+        //[PrimeraLetraMayuscula]
         public string Nombre { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Nombre))
+            {
+                var primeraLetra = Nombre[0].ToString();
+
+                if (primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayuscula",
+                    new string[] { nameof(Nombre) }); //a que campo le corresponde la validacion
+                }
+            }
+        }
     }
 }
--- back-end/back-end/README.md
+++ back-end/back-end/README.md
@@ -1,19 +1,6 @@
-﻿## 77 - Validando Modelos
+﻿## 78 - Validaciones Personalizadas
 
-
-Validando Modelos
-
--Podemos utilizar atributos para realizar validaciones
-
--Required  -->El campo es requerido.
--StringLength -->Indica la longitud que debe tener un string.
--Range -->Rango de un valor numerico.
--CreditCard -->Validar tarjetas de credito(Que la numeracion es valida), pero no que esta activa o inactiva.
--Compare -->Valida que dos propiedades son iguales
--Phone--> Valida que un telefono es correcto
--RegularExpresion -->Sirve para validar contra una expresion regular
--Url -->Valida un formato de url
--BindRequired -->Indica que se requiere n binding hacia la propiedad
-
+Hemos implementado dos tipos de validaciones, la primera que es validacion por atributoi mediante la creacion
+de una clase, la validacion por modelo la hacve implementando la interfaz IValidatableObject.
 
 
